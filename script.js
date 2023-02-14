9ac8fdd06c6df25c3b8b1183b4397ce175d8cb38
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-02-14 14:37:18 +0800

    + - * /

---
+++ script.js
@@ -0,0 +1,13 @@
+//plus minus multiply division
+function add(a ,b){
+    return a+b;
+}
+function minus(a ,b){
+    return a-b;
+}
+function multiply(a ,b){
+    return a*b;
+}
+function division(a ,b){
+    return a/b;
+}
