10a2dfc19e52725d7cdcd8f4f834cb9a3223f2b1
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-02-17 23:13:56 +0800

    basic function

--- script.js
+++ script.js
@@ -21,13 +21,45 @@ function operator(operator ,a , b){
         case '*':
             return multiply(a ,b);
         case '/':
-            return devision(a ,b);
+            return division(a ,b);
         default :
             return null;
     }
 }
+
 //button
-let btn = document.querySelectorAll('button');
+let btn = document.querySelectorAll('.btnN');
+let displayInput = document.getElementById('displayInput');
+let displayOutput = document.getElementById('displayOutput');
+let isEqualsClicked = false;   // set a variable to track if clicked
+
 btn.forEach((callback)=>callback.addEventListener('click',(e)=>{
-    console.log(e.target.textContent);
-}))
+    displayInput.textContent += e.target.textContent ;
+    isEqualsClicked = false;     
+}));
+
+let btnO = document.querySelectorAll('.btnO');
+btnO.forEach((callback)=>callback.addEventListener('click',(e)=>{
+    displayOutput.textContent=  displayInput.textContent+ e.target.textContent;
+    displayInput.textContent = '';
+    isEqualsClicked = false;
+}));
+
+let btnE = document.querySelector('.btnE');
+btnE.addEventListener('click',()=>{
+    if (isEqualsClicked) return;     //if is true then skip below
+    let OP = displayOutput.textContent.slice(-1);
+    let a = parseFloat(displayOutput.textContent.slice(0,-1));
+    let b = parseFloat(displayInput.textContent);
+    displayOutput.textContent = displayOutput.textContent + displayInput.textContent+'=';
+    displayInput.textContent = operator(OP,a,b);
+    isEqualsClicked = true;      // set to true if click 
+}); 
+
+//clear
+let btnC = document.querySelector('.btnC');
+btnC.addEventListener('click',()=>{
+    displayInput.textContent ='';
+    displayOutput.textContent='';
+    isEqualsClicked = false;
+});
